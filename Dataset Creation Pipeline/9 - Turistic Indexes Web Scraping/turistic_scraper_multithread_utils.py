af292dd61958e487b11f81ccc06ca35d14575473
Marco Terzulli <terzulli.marco9@gmail.com>
2022-07-28 19:28:55 +0200

    Analisi Turistica: inizio lavoro web scraper per wikipedia SWP

---
+++ Dataset Creation Pipeline/9 - Turistic Indexes Web Scraping/turistic_scraper_multithread_utils.py
@@ -0,0 +1,36 @@
+# *********** TEST ************ #
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import numpy as np
+from time import sleep
+import requests
+from urllib.request import urlopen
+
+# compute the city SWP from wikipedia
+def get_city_swp_v1(url):
+    page = requests.head(url)
+    c=0
+
+    while page.headers.get ( "content-length" ) == None:
+        if c < 4:
+            page = requests.head(url)
+        else: 
+            break
+        c += 1
+
+    return page.headers.get("content-length")
+
+# Prepare the URL and compute the city SWP from wikipedia
+def mt_get_city_swp_operation_v1(conf_location, wikipedia_url, sleep_delay=0):
+    try:
+        sleep(sleep_delay)
+        url = wikipedia_url + conf_location[1] # example: "https://en.wikipedia.org/wiki/Shijiazhuang"
+    except:
+        pass
+    try:
+        swp = get_city_swp_v1(url)
+    except:
+        swp = np.nan
+    return conf_location, swp
+
